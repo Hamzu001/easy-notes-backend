ae17028541d9cc1bd58da876b05db58fc9a5f395
Hamzu001 <zahidhamza001@gmail.com>
2023-03-08 16:30:55 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://0.0.0.0:27017/Hamza";
+const mongoURI = process.env.DB;
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
         console.log("Connect to mongoose succesfully");
